2870b74b1696195048952083db8b93fce5ef6823
sevenc-nanashi <sevenc7c@sevenc7c.com>
2026-08-18 09:57:57 +0900

    feat: ciでupdate.rbを走らせるようにする

--- update.rb
+++ update.rb
@@ -2,11 +2,16 @@
 require "English"
 
 version = ARGV[0]
+if !version.match?(/\A[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+?\z/)
+  raise "Invalid version format: #{version}"
+end
+
+sourcemonitor_version = version.match(/\A([0-9]+\.[0-9]+\.[0-9]+)\.[0-9]+?\z/)[1]
 
 aviutl2_toml = File.read("./aviutl2.toml", mode: "rb")
-aviutl2_toml.gsub!(/\/v([0-9]+\.[0-9]+\.[0-9]+)/, "\/v#{version}") or raise "Unreachable: No replacement"
+aviutl2_toml.gsub!(/\/v([0-9]+\.[0-9]+\.[0-9]+)/, "\/v#{sourcemonitor_version}") or raise "Unreachable: No replacement"
 old_version = Regexp.last_match[1]
-aviutl2_toml.gsub!(/version = "[0-9]+\.[0-9]+\.[0-9]+(\.[0-9]+)"/, "version = \"#{version}\\1\"") or raise "Unreachable: No replacement"
+aviutl2_toml.gsub!(/version = "[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+"/, "version = \"#{version}\"") or raise "Unreachable: No replacement"
 
 File.write("aviutl2.toml", aviutl2_toml, mode: "wb")
 
